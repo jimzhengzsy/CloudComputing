11a177b2aed690a3e77275050be56a0c75458dde
jimzhengzsy <70294267+jimzhengzsy@users.noreply.github.com>
2023-02-25 05:32:02 -0600

    a13

--- ann/ann_config.py
+++ ann/ann_config.py
@@ -1,45 +1,58 @@
-# ann_config.ini
+# ann_config.py
 #
 # Copyright (C) 2011-2022 Vas Vasiliadis
 # University of Chicago
 #
-# GAS annotator configuration
+# Set GAS annotator configuration options
 #
 ##
+__author__ = 'Vas Vasiliadis <vas@uchicago.edu>'
 
-# AnnTools settings
-[ann]
+import os
+class Config(object):
 
-# AWS general settings
-[aws]
-AwsRegionName = us-east-1
+  # Get configuration
+  from configparser import ConfigParser
+  config = ConfigParser(os.environ)
+  config.read('app_config.ini')
 
-# AWS SQS queues
-[sqs]
-# AWS SQS queue for A10
-AWS_SQS_Queue_NAME_A10 = https://sqs.us-east-1.amazonaws.com/127134666975/songyuanzheng_a10_job_requests
-# AWS SQS queue for A11
-AWS_SQS_QUEUE_URL_A11 = https://sqs.us-east-1.amazonaws.com/127134666975/songyuanzheng_a11_job_requests
+  CSRF_ENABLED = True
 
-# AWS SQS queue 
-AWS_SQS_QUEUE_URL = https://sqs.us-east-1.amazonaws.com/127134666975/songyuanzheng_a13_job_requests
+  ANNOTATOR_BASE_DIR = "/home/ubuntu/gas/ann/"
+  ANNOTATOR_JOBS_DIR = "/home/ubuntu/gas/ann/jobs"
 
-# AWS S3
-[s3]
-Result_Bucket = gas-results
-# AWS SNS topics
-[sns]
-# AWS SNS topic for A10
-AWS_SNS_ARN_TOPIC_A10 = arn:aws:sns:us-east-1:127134666975:songyuanzheng_a10_job_requests
-# AWS SNS topic for A11
-AWS_SNS_ARN_TOPIC_A11 = arn:aws:sns:us-east-1:127134666975:songyuanzheng_a11_job_requests
+  AWS_REGION_NAME = "us-east-1"
 
-# AWS SNS topic
-AWS_SNS_ARN_TOPIC = arn:aws:sns:us-east-1:127134666975:songyuanzheng_a13_job_requests
-# AWS DynamoDB
-[dynamodb]
-AWS_DYNAMODB_ANNOTATIONS_TABLE = songyuanzheng_annotations
+  # AWS S3 upload parameters
+  AWS_S3_INPUTS_BUCKET = "gas-inputs"
+  AWS_S3_RESULTS_BUCKET = "gas-results"
 
+  # AWS SNS topics
 
+  # AWS SNS topic for A10
+  AWS_SNS_ARN_TOPIC_A10 = 'arn:aws:sns:us-east-1:127134666975:songyuanzheng_a10_job_requests'
+  # AWS SNS topic for A11
+  AWS_SNS_ARN_TOPIC_A11 = 'arn:aws:sns:us-east-1:127134666975:songyuanzheng_a11_job_requests'
+
+  # AWS SNS topic
+  AWS_SNS_ARN_TOPIC = 'arn:aws:sns:us-east-1:127134666975:songyuanzheng_a13_job_requests'
+
+  # AWS SQS queues
+  AWS_SQS_WAIT_TIME = 20
+  AWS_SQS_MAX_MESSAGES = 10
+
+  # AWS DynamoDB
+  AWS_DYNAMODB_ANNOTATIONS_TABLE = "songyuanzheng_annotations"
+
+  # AWS SNS topic for A10
+  AWS_SNS_ARN_TOPIC_A10 = 'arn:aws:sqs:us-east-1:127134666975:songyuanzheng_a10_job_requests'
+
+  # AWS SQS queue for A10
+  AWS_SQS_Queue_NAME_A10 = 'https://sqs.us-east-1.amazonaws.com/127134666975/songyuanzheng_a10_job_requests'
+  # AWS SQS queue for A11
+  AWS_SQS_QUEUE_URL_A11 = 'https://sqs.us-east-1.amazonaws.com/127134666975/songyuanzheng_a11_job_requests'
+
+  # AWS SQS queue 
+  AWS_SQS_QUEUE_URL = 'https://sqs.us-east-1.amazonaws.com/127134666975/songyuanzheng_a13_job_requests'
 
 ### EOF
